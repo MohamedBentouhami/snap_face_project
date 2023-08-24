1b747f7b129057934ddcb06a7514d0849650ba1b
Mohamed Bentouhami Belhaj <56387@etu.he2b.be>
2023-08-24 21:39:40 +0200

    add views

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl! : string;
+export class FaceSnapComponent{
+  @Input() faceSnap! : FaceSnap;
+  buttonText: string = "Oh Snap!";
 
-  ngOnInit(): void {
-    this.title = 'Archibald';
-    this.description = 'Mon meilleur ami depuis tout petit';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+
+  onAddSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, un snap!'
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap!'
+    }
   }
 
 }
